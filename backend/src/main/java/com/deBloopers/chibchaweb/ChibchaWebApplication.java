b1e46c98a296bcf7bbf0968f44dd7af5ab9413b0
Cristian Rios <40129020+sanxxtiago@users.noreply.github.com>
2025-07-22 15:25:04 -0500

    Added backend & frontend project templates
    + Added Spring boot with dependencies
    + Added React app with dependencies

---
+++ backend/src/main/java/com/deBloopers/chibchaweb/ChibchaWebApplication.java
@@ -0,0 +1,13 @@
+package com.deBloopers.chibchaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChibchaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChibchaWebApplication.class, args);
+	}
+
+}
